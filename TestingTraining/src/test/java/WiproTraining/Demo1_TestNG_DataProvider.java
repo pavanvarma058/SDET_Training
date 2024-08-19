0e52ad0af89e58a2a622c7d7a5593de3881ef0d2
Pavan Varma <pavanvarma058@gmail.com>
2024-08-19 22:34:00 +0530

    Learned about before and after methods in testNG

---
+++ TestingTraining/src/test/java/WiproTraining/Demo1_TestNG_DataProvider.java
@@ -0,0 +1,11 @@
+package WiproTraining;
+
+import org.testng.annotations.Test;
+
+public class Demo1_TestNG_DataProvider {
+	
+	@Test
+	public void test1(int n) {
+		System.out.println("value of n= " + n);
+	}
+}
