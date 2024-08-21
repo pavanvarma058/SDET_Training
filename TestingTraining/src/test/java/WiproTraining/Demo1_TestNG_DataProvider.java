3605ea918a4b4d10783d0051e5466d5ebe30f178
Pavan Varma <pavanvarma058@gmail.com>
2024-08-21 23:34:47 +0530

    added practiced testNG files

--- TestingTraining/src/test/java/WiproTraining/Demo1_TestNG_DataProvider.java
+++ TestingTraining/src/test/java/WiproTraining/Demo1_TestNG_DataProvider.java
@@ -1,11 +1,26 @@
 package WiproTraining;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Demo1_TestNG_DataProvider {
 	
-	@Test
+	//if you want to execute same test case multiple times for different different input then we should go for 
+	// parameterized testing
+	
+	@Test (dataProvider = "tdata")
 	public void test1(int n) {
 		System.out.println("value of n= " + n);
 	}
+	
+	// we can provide input test data to the test case using DataProvider
+	// DataProvider  is a method by which we can supply or provide input test data to the test case method
+	
+	//Mark a method as supplying data for a test method
+	@DataProvider (name="tdata")
+	public Integer[] getData() {
+		Integer[] arr = {10,20,3,45,6,86};
+		return arr;
+	}
 }
+
